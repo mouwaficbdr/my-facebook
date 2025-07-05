f8d6456e8bff1306d894898e49b9e09e3e3283b3
mouwaficbdr <badaroumouwafic@gmail.com>
2025-07-05 12:10:53 +0100

    Refactor App component to include AuthProvider and update routing. Added routes for login, register, forgot password, reset password, and home, improving user navigation and authentication flow.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,25 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import Register from './pages/Register';
+import Login from './pages/Login';
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Register />} />
-        {/* <Route path="/login" element={<Login />} />
-        <Route path="/reset-password" element={<ResetPassword />} /> */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
