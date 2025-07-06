85d0a3baed47d70caf7c4c9cbd91b5294244feaa
mouwaficbdr <badaroumouwafic@gmail.com>
2025-07-06 18:12:33 +0100

    Ajoute des variables d'environnement pour la configuration PHP dans start-container.sh, activant l'affichage des erreurs et la journalisation, afin d'améliorer le débogage de l'application.

--- backend/start-container.sh
+++ backend/start-container.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -x
 
+export PHP_INI_SCAN_DIR=/app/backend/
+export PHP_INI_DIR=/app/backend/
+export PHP_ERROR_REPORTING=E_ALL
+export PHP_DISPLAY_ERRORS=On
+export PHP_LOG_ERRORS=On
+export PHP_ERROR_LOG=/dev/stderr
+
 # Démarrer FrankenPHP avec le Caddyfile
 frankenphp run --config Caddyfile
